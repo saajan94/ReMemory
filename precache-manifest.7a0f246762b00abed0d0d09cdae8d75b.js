20fac1320908c2fde9dbab1cca14ee1c4658917f
saajan94 <saajan1994@aol.com>
2018-10-04 21:36:38 -0400

    Updates

---
+++ precache-manifest.7a0f246762b00abed0d0d09cdae8d75b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7212146f2e38ffc16216",
+    "url": "/ReMemory/static/js/runtime~main.7212146f.js"
+  },
+  {
+    "revision": "c51225c5aefd0a559758",
+    "url": "/ReMemory/static/js/main.c51225c5.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/ReMemory/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "c51225c5aefd0a559758",
+    "url": "/ReMemory/static/css/main.dc1cf67b.chunk.css"
+  },
+  {
+    "revision": "1452d58373e96ea86d511785dbcbb948",
+    "url": "/ReMemory/index.html"
+  }
+];
